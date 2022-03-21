2dbbba8aa7c040c75e7a1e3b309a55a69c990aec
ines.magalhaes <ines.magalhaes@olx.com>
2022-03-21 12:07:42 +0000

    [NOJIRA] update styles of mini-olx

--- mini-olx-frontend/src/App.js
+++ mini-olx-frontend/src/App.js
@@ -1,13 +1,61 @@
+import PostingForm from "./components/PostingForm/PostingForm";
+import {useEffect, useState} from "react";
+import Ads from "./components/Ads/Ads";
+import config from "./config";
+import {Divider} from "antd";
+import Title from "antd/lib/typography/Title";
+import Layout, {Content, Footer, Header} from "antd/lib/layout/layout";
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Mini OLX</h1>
-      </header>
-    </div>
-  );
+
+    const [ads, setAds] = useState([]);
+
+    const fetchAllAds = () => {
+        fetch(config.api_ads)
+            .then((res) => res.json())
+            .then((response) => setAds(response.data))
+            .catch((err) => console.error(err));
+    };
+
+    const handleOnDelete = (adId) => {
+        if (!adId) {
+            return;
+        }
+        fetch(`${config.api_ads}/${adId}`, {method: "DELETE"})
+            .then((res) => res.json())
+            .then((response) => fetchAllAds())
+            .catch((err) => console.error(err));
+    };
+
+
+    useEffect(() => {
+        fetchAllAds();
+    }, []);
+
+    const handleOnPostAd = () => {
+        fetchAllAds();
+    };
+
+
+    return (
+        <Layout>
+            <Header>
+                <Title className="header__title">Mini OLX</Title>
+            </Header>
+            <Content>
+                <div className="App">
+                    <main>
+                        <PostingForm onPostAd={handleOnPostAd}/>
+                        <Divider/>
+                        <Ads ads={ads} onDeleteAd={handleOnDelete}/>
+                    </main>
+                </div>
+            </Content>
+            <Footer>Ines @ OLX Workshop ©2022</Footer>
+        </Layout>
+    );
 }
 
 export default App;
