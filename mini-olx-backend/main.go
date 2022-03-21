ebaf7649c1217b8fd9a79816c9def390d950eb2b
ines.magalhaes <ines.magalhaes@olx.com>
2022-03-21 17:29:42 +0000

    [NOJIRA] add go part to mini-olx

--- mini-olx-backend/main.go
+++ mini-olx-backend/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"mini-olx-backend/database"
+	"mini-olx-backend/handlers"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/rs/cors"
+)
 
 func main() {
-    fmt.Println("Hello World!")
+	database.CreateConn()
+
+	router := mux.NewRouter()
+	router.HandleFunc("/api/ads", handlers.List).Methods(http.MethodGet)
+	router.HandleFunc("/api/ads", handlers.Create).Methods(http.MethodPost)
+	router.HandleFunc("/api/ads/{id}", handlers.Delete).Methods(http.MethodDelete)
+
+	router.
+		PathPrefix("/static/").
+		Handler(http.StripPrefix("/", http.FileServer(http.Dir("./static/"))))
+
+	allowedMethods := []string{
+		http.MethodGet,
+		http.MethodPost,
+		http.MethodDelete,
+		http.MethodPatch,
+		http.MethodOptions,
+	}
+
+	handler := cors.
+		New(cors.Options{AllowedMethods: allowedMethods}).
+		Handler(router)
+
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), handler)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
